895e63d954ee290a4b4cad5cda2aa62a604869e8
zolozAdmin <zoloz-github@antgroup.com>
2020-10-15 15:29:39 +0800

    initialize project

---
+++ app/src/main/java/com/zoloz/saas/example/LocalRequest.java
@@ -0,0 +1,74 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2020 ZOLOZ-PTE-LTD
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zoloz.saas.example;
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LocalRequest implements IRequest {
+    public String request(String requestUrl, String requestData) {
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            //设置参数
+            httpConn.setDoOutput(true);     //需要输出
+            httpConn.setDoInput(true);      //需要输入
+            httpConn.setUseCaches(false);   //不允许缓存
+            httpConn.setRequestMethod("POST");      //设置POST方式连接
+            //设置请求属性
+            httpConn.setRequestProperty("Content-Type", "application/json");
+            httpConn.setRequestProperty("Connection", "Keep-Alive");// 维持长连接
+            httpConn.setRequestProperty("Charset", "UTF-8");
+
+            //连接,也可以不用明文connect，使用下面的httpConn.getOutputStream()会自动connect
+            httpConn.connect();
+
+            //建立输入流，向指向的URL传入参数
+            DataOutputStream dos = new DataOutputStream(httpConn.getOutputStream());
+            dos.writeBytes(requestData);
+            dos.flush();
+            dos.close();
+            //获得响应状态
+            int resultCode = httpConn.getResponseCode();
+            if (HttpURLConnection.HTTP_OK == resultCode) {
+                StringBuffer sb = new StringBuffer();
+                String readLine = new String();
+                BufferedReader responseReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8"));
+                while ((readLine = responseReader.readLine()) != null) {
+                    sb.append(readLine).append("\n");
+                }
+                responseReader.close();
+                System.out.println(sb.toString());
+                return sb.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
---
+++ app/src/main/java/com/zoloz/saas/example/MainActivity.java
@@ -0,0 +1,218 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2020 ZOLOZ-PTE-LTD
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zoloz.saas.example;
+
+
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import com.ap.zoloz.hummer.api.IZLZCallback;
+import com.ap.zoloz.hummer.api.ZLZConstants;
+import com.ap.zoloz.hummer.api.ZLZFacade;
+import com.ap.zoloz.hummer.api.ZLZRequest;
+import com.ap.zoloz.hummer.api.ZLZResponse;
+import com.zoloz.builder.BuildConfig;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+
+    public static final String DEFAULT_URL = "http://<lan_ip>:8080/api/realid/initialize";
+
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHandler = new Handler();
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(this,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        EditText editText = findViewById(R.id.init_url);
+        editText.setText(getSaveUrl(getDefaultUrl()));
+
+    }
+
+
+    protected String getDefaultUrl() {
+        return DEFAULT_URL;
+    }
+
+
+    private String getSaveUrl(String defaultUrl) {
+        SharedPreferences preferences = getSharedPreferences("url", MODE_PRIVATE);
+        return preferences.getString(this.getClass().getName(), defaultUrl);
+    }
+
+    private void saveUrl(String url) {
+        SharedPreferences preferences = getSharedPreferences("url", MODE_PRIVATE);
+        preferences.edit().putString(this.getClass().getName(), url).commit();
+    }
+
+
+    private void runOnIoThread(Runnable runnable) {
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+
+    public void startZoloz(View view) {
+        runOnIoThread(new Runnable() {
+            @Override
+            public void run() {
+                String result = mockInitRequest();
+                if (TextUtils.isEmpty(result)) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "network exception, please try again later.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    return;
+                }
+                final InitResponse initResponse = JSON.parseObject(result, InitResponse.class);
+                final ZLZFacade zlzFacade = ZLZFacade.getInstance();
+                final ZLZRequest request = new ZLZRequest();
+                request.zlzConfig = initResponse.clientCfg;
+                request.bizConfig.put(ZLZConstants.CONTEXT, MainActivity.this);
+                request.bizConfig.put(ZLZConstants.PUBLIC_KEY, initResponse.rsaPubKey);
+                request.bizConfig.put(ZLZConstants.LOCALE, "en-US");
+                Log.d(TAG, "request success:");
+                mHandler.postAtFrontOfQueue(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(TAG, "start zoloz");
+                        zlzFacade.start(request, new IZLZCallback() {
+                            @Override
+                            public void onCompleted(ZLZResponse response) {
+                                showResponse(initResponse.transactionId, response);
+                            }
+
+                            @Override
+                            public void onInterrupted(ZLZResponse response) {
+                                showResponse(initResponse.transactionId, response);
+                                Toast.makeText(MainActivity.this, "interrupted", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private void showResponse(final String flowId, ZLZResponse response) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("Result")
+                .setMessage(JSON.toJSONString(response)
+                )
+                .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ClipboardManager myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                        ClipData myClip;
+                        String text = flowId;
+                        myClip = ClipData.newPlainText("text", text);
+                        myClipboard.setPrimaryClip(myClip);
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        alertDialog.setCancelable(false);
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
+    }
+
+    protected String mockInitRequest() {
+        IRequest request = generateRequest();
+        String result = request.request(getUrl(), generateRequestData());
+        return result;
+    }
+
+    protected IRequest generateRequest() {
+        return new LocalRequest();
+    }
+
+    protected String getUrl() {
+        EditText editText = findViewById(R.id.init_url);
+        String url = editText.getText().toString();
+        if (TextUtils.isEmpty(url)) {
+            return getDefaultUrl();
+        } else {
+            saveUrl(url);
+            return url;
+        }
+    }
+
+    public String generateRequestData() {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("bizId", "test-bizId");
+            jsonObject.put("userId", "216610000001376836453");
+            jsonObject.put("sceneCode", "registration");
+            jsonObject.put("metaInfo", ZLZFacade.getMetaInfo(this));
+            jsonObject.put("v", BuildConfig.VERSION_NAME);
+            JSONObject envData = new JSONObject();
+            envData.put("envName", "default");
+            jsonObject.put("envData", envData);
+            return jsonObject.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+}
